0caff9c2c92627df536178cd7d213e15fe4e8f73
Karen <khambro@gmail.com>
2015-04-24 14:45:47 -0700

    adds create job action.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,15 @@ class JobsController < ApplicationController
     @job=Job.new
   end
 
+  def create
+    Job.create(job_params)
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title,:description, :location, :pay)
+  end
+
 end
